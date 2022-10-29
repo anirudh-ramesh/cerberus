bad80b6badad9ced01d442e6c8de18b7227623d7
Anirudh Ramesh <anirudh@irasus.com>
2022-10-29 20:25:35 +0530

    Add alternative BASE_DIR determination method

--- cerberus/cerberus_django/settings.py
+++ cerberus/cerberus_django/settings.py
@@ -18,6 +18,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 #
 import os
 
+# # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
