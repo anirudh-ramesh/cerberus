ad0ea14f1826ea96775c7a30d7c314e70fa20d15
dixitims <dixit.ims.in@gmail.com>
2022-11-14 18:24:45 +0530

    update irasus.sh file

--- cerberus/irasus.sh
+++ cerberus/irasus.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 pip install -r requirements.txt
+sudo apt install postgis postgresql-12-postgis-2.5
+touch user_management/migrations/_init_it.py
 python3 manage.py makemigrations
 python3 manage.py migrate
 
